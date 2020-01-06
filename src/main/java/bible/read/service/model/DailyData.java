7db8d30130d30283948dc55f89775338976fd95b
renceabi <renceabishek@gmail.com>
2020-01-06 21:52:51 +0530

    checking second commit

--- src/main/java/bible/read/service/model/DailyData.java
+++ src/main/java/bible/read/service/model/DailyData.java
@@ -1,16 +1,7 @@
 package bible.read.service.model;
 
 import com.google.firebase.database.Exclude;
-import java.time.LocalDate;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Builder
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class DailyData {
 
   @Exclude
@@ -22,4 +13,73 @@ public class DailyData {
   private int fromVerses;
   private int toVerses;
 
+  public DailyData() {
+  }
+
+  public DailyData(String uniqueId, String date, String name, String portion,
+      String chapter, int fromVerses, int toVerses) {
+    this.uniqueId = uniqueId;
+    this.date = date;
+    this.name = name;
+    this.portion = portion;
+    this.chapter = chapter;
+    this.fromVerses = fromVerses;
+    this.toVerses = toVerses;
+  }
+
+  public String getUniqueId() {
+    return uniqueId;
+  }
+
+  public void setUniqueId(String uniqueId) {
+    this.uniqueId = uniqueId;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPortion() {
+    return portion;
+  }
+
+  public void setPortion(String portion) {
+    this.portion = portion;
+  }
+
+  public String getChapter() {
+    return chapter;
+  }
+
+  public void setChapter(String chapter) {
+    this.chapter = chapter;
+  }
+
+  public int getFromVerses() {
+    return fromVerses;
+  }
+
+  public void setFromVerses(int fromVerses) {
+    this.fromVerses = fromVerses;
+  }
+
+  public int getToVerses() {
+    return toVerses;
+  }
+
+  public void setToVerses(int toVerses) {
+    this.toVerses = toVerses;
+  }
 }
